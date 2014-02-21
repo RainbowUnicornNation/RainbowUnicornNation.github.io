cc5088b70a2edf58e0894b4310d4d2ea84e43a7b
yanbro94 <yannick.r.brodard@gmail.com>
2014-02-21 15:22:11 +0100

    update

--- footer.php
+++
@@ -1,3 +0,0 @@
-</div> <!-- END OF MAIN DIV -->
-</body>
-</html
--- index.php
+++ index.php
@@ -1,7 +1,33 @@
-<?php
-
-include_once 'header.php';
-include_once 'front-page.php';
-include_once 'footer.php';
-
-?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="./css/main.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <div id="main">
+            <header>
+            	<img id="constr" src="./imgs/underconstruction.png" alt="Website under construction"/>
+                <div id="headMessage"><p>Careful under the rainbow...</p></div>
+                <img id="logo" src="./imgs/logo-run-tmp_small.png" alt="RUN logo" height="84" width="221"/>
+                <nav id="mainNav">
+                    <div id="navHome">
+                    	<a href="index.php" alt="Home">Home</a>
+                    	<a href="index.php" alt="Games">Games</a>
+                    	<a href="index.php" alt="Apps">Apps</a>
+                    	<a href="index.php" alt="Others">Others</a>
+                    </div>
+                </nav>
+            </header>
+        <div id="mainIMGs">
+    <img src="./imgs/IMGMAIN-run-tmp.png" alt="" border="1"/>
+    <div id="mainIMGsContent">
+        <h1 class="frontpage1">We translate dreams into code</h1>
+        <p class="frontpage2">Dreams become true in RUN and we make sure that they'll make you smile (at least). :)</p>
+    </div>
+</div>
+</div> <!-- END OF MAIN DIV -->
+</body>
+</html
